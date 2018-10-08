d48a3131cbe5cf3ea6b5bea10caa217875d99781
Denis Yılmaz <mail@denisyilmaz.de>
2018-10-08 09:49:32 +0200

    update deps to support angular 6 and add prettier for file formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { ContentfulModule } from './modules/contentful/contentful.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { ContentfulModule } from "./modules/contentful/contentful.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     ContentfulModule.forRoot({
-      space: 'yadj1kx9rmg0',
-      accessToken: 'fdb4e7a3102747a02ea69ebac5e282b9e44d28fb340f778a4f5e788625a61abe',
-    }),
+      space: "yadj1kx9rmg0",
+      environment: "master",
+      accessToken:
+        "fdb4e7a3102747a02ea69ebac5e282b9e44d28fb340f778a4f5e788625a61abe"
+    })
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
